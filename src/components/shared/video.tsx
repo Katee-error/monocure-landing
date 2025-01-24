2977172f5150c6116f15b24c2db0f3570d9ae23c
Ekaterina <kate8190758@gmail.com>
2025-01-24 15:12:51 +0400

    change controls video

--- src/components/shared/video.tsx
+++ src/components/shared/video.tsx
@@ -1,32 +1,23 @@
-'use client'
 import { Box, AspectRatio, Heading } from "@chakra-ui/react";
-import { useState } from "react";
 
 const VideoReview = () => {
-  const [controlsVisible, setControlsVisible] = useState(false);
-
-  const handleVideoClick = () => {
-    setControlsVisible(true); // Показываем элементы управления при клике
-  };
-
   return (
     <Box bg="white" mb="40px" borderRadius="md" p={4} textAlign="center" mt={8}>
       <Heading textAlign="center" mb={["20px", "40px"]}>
         Video Übersicht
       </Heading>
-      <AspectRatio ratio={16 / 9} maxW={['100%', '80%']} mx="auto">
+      <AspectRatio ratio={16 / 9} maxW={["100%", "80%"]} mx="auto">
         <video
           title="Video Übersicht"
           src="/assets/video/monocure.mp4"
           muted
           autoPlay
           loop
-          controls={controlsVisible} // Показываем элементы управления, если кликнули
-          onClick={handleVideoClick} // При клике показываем элементы управления
+          controls
           style={{
-            width: '100%',
-            maxWidth: '100%',
-            objectFit: 'cover',
+            width: "100%",
+            maxWidth: "100%",
+            objectFit: "cover",
           }}
         />
       </AspectRatio>
@@ -35,4 +26,3 @@ const VideoReview = () => {
 };
 
 export default VideoReview;
-
