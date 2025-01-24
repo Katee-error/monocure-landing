a9b3e056f871608e4e9c378f9383f9f4773303d6
Ekaterina <kate8190758@gmail.com>
2025-01-24 14:58:27 +0400

    chamge video controls

--- src/components/shared/video.tsx
+++ src/components/shared/video.tsx
@@ -1,20 +1,33 @@
-import { Box, AspectRatio, Text, Heading } from "@chakra-ui/react";
+'use client'
+import { Box, AspectRatio, Heading } from "@chakra-ui/react";
+import { useState } from "react";
 
 const VideoReview = () => {
+  const [controlsVisible, setControlsVisible] = useState(false);
+
+  const handleVideoClick = () => {
+    setControlsVisible(true); // Показываем элементы управления при клике
+  };
+
   return (
-    <Box bg="white" mb='40px' borderRadius="md" p={4} textAlign="center" mt={8}>
+    <Box bg="white" mb="40px" borderRadius="md" p={4} textAlign="center" mt={8}>
       <Heading textAlign="center" mb={["20px", "40px"]}>
-      Video Übersicht
+        Video Übersicht
       </Heading>
       <AspectRatio ratio={16 / 9} maxW={['100%', '80%']} mx="auto">
-      <video
+        <video
           title="Video Übersicht"
           src="/assets/video/monocure.mp4"
-          controls
-          muted 
+          muted
           autoPlay
           loop
-          style={{ width: '100%', objectFit: 'cover' }}
+          controls={controlsVisible} // Показываем элементы управления, если кликнули
+          onClick={handleVideoClick} // При клике показываем элементы управления
+          style={{
+            width: '100%',
+            maxWidth: '100%',
+            objectFit: 'cover',
+          }}
         />
       </AspectRatio>
     </Box>
@@ -22,3 +35,4 @@ const VideoReview = () => {
 };
 
 export default VideoReview;
+
