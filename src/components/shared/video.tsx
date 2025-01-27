b38df5c988deb8e5fd36e9be1e9a2ec47fad7b76
Ekaterina <kate8190758@gmail.com>
2025-01-28 03:23:00 +0400

    fix: change mobile size video componet

--- src/components/shared/video.tsx
+++ src/components/shared/video.tsx
@@ -1,20 +1,43 @@
-import { Box, AspectRatio, Flex } from "@chakra-ui/react";
+'use client'
+import {Flex, useMediaQuery } from "@chakra-ui/react";
 
 export const VideoReview = () => {
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
   return (
-    <Flex justifyContent='center' bg="white" mb="40px" borderRadius="20px" p={4} textAlign="center" mt={8} >
-      <iframe
-            width="80%" 
-            height="auto" 
-            style={{
-              aspectRatio: "16/9", 
-              borderRadius: '20px'
-            }}
-            src="https://www.youtube.com/embed/JM2DIgF8Cug?playlist=JM2DIgF8Cug&autoplay=1&loop=1&mute=1&modestbranding=1&controls=1"
-            allow="autoplay; encrypted-media"
-            allowFullScreen
-       
-      />
+    <Flex
+      justifyContent="center"
+      bg="white"
+      mb="40px"
+      borderRadius="20px"
+      p={4}
+      textAlign="center"
+      mt={8}
+    >
+      {isMobile ? (
+        <iframe
+          width="100%"
+          height="auto"
+          style={{
+            aspectRatio: "16/9",
+            borderRadius: "20px",
+          }}
+          src="https://www.youtube.com/embed/JM2DIgF8Cug?playlist=JM2DIgF8Cug&autoplay=1&loop=1&mute=1&modestbranding=1&controls=1"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        />
+      ) : (
+        <iframe
+          width="80%"
+          height="auto"
+          style={{
+            aspectRatio: "16/9",
+            borderRadius: "20px",
+          }}
+          src="https://www.youtube.com/embed/JM2DIgF8Cug?playlist=JM2DIgF8Cug&autoplay=1&loop=1&mute=1&modestbranding=1&controls=1"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        />
+      )}
     </Flex>
   );
 };
