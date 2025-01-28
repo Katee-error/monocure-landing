3f51fea6a796aa57e12bd80079433ac2760587a8
Ekaterina <kate8190758@gmail.com>
2025-01-28 18:02:08 +0400

    fix return image before component

--- src/components/shared/application.tsx
+++ src/components/shared/application.tsx
@@ -74,7 +74,7 @@ export const Application: React.FC<Props> = ({ className }) => {
   return (
     <Box
       id="section4"
-      py={["40px", "60px", "80px"]}
+      py={["40px", "60px", "40px"]}
       ref={ref}
       overflow="hidden"
       opacity={isVisible ? 1 : 0}
